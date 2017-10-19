f98842ef26ada3c75ab1ea37e0371f2a0b0263a0
Unknown <10798368@my.uvu.edu>
2017-10-18 21:50:14 -0600

    chicken movement
    changes added to make chicken follow player

---
+++ Chicken_Game/Assets/Scripts/ChickenFollow.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChickenFollow : MonoBehaviour {
+
+	public Transform target;
+	public Transform myTransform;
+
+
+void Start () {
+
+target = GameObject.FindWithTag("Player").transform;
+//Allows chicken to find player, or else it just wanders off into the void.
+
+}
+
+void Update () {
+
+transform.LookAt (target);
+transform.Translate (Vector3.forward * Time.deltaTime);
+
+/*Set RigidBody on Chicken, set mass to .02 or something small, and set player mass higher
+to keep chicken from pushing player. Mesh Colliders on both Player and Chicken keep them
+from passing through one another.*/
+
+
+}
+
+
+
+
+}
+
