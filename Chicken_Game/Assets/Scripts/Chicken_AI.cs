4207a86441f09c0784b102b2674eb1c7d7376b09
Unknown <10798368@my.uvu.edu>
2017-10-25 20:34:06 -0600

    chicken pen teleport
    chicken pen added, but wandering AI causes the player to be launched

--- Chicken_Game/Assets/Scripts/Chicken_AI.cs
+++ Chicken_Game/Assets/Scripts/Chicken_AI.cs
@@ -2,11 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent (typeof(CharacterController))]
 public class Chicken_AI : MonoBehaviour {
 
 	public Rigidbody enemy;
 	public float moveSpeed;
 	public Transform target;
+	public Transform chickenPen;
+
+public float speed = 5;
+public float directionChangeInterval = 1;
+public float maxHeadingChange = 30;
+
+CharacterController controller;
+float heading;
+Vector3 targetRotation;
 
 /*Apply Box Collider to Chicken and check "Is Trigger". Collider will
 bounce off contact, walls, floor, etc. Triggers allow passing through.*/
@@ -22,14 +32,27 @@ bounce off contact, walls, floor, etc. Triggers allow passing through.*/
 
 	//Discriminate what the chicken's trigger will respond to.
 
-	if (other.gameObject.name == "Player") {
-		Debug.Log ("Player has entered the chicken's trigger.");
-		transform.LookAt (target);
-		transform.Translate (Vector3.forward * - moveSpeed * Time.deltaTime);
+		if (other.gameObject.name == "Player") {
+			Debug.Log ("Player has entered the chicken's trigger.");
+			transform.LookAt (target);
+			transform.Translate (Vector3.forward * - moveSpeed * Time.deltaTime);
 
 		// - before moveSpeed causes the chicken to run away from the player.
 		//Alternatively Vector3.back will cause a negative movement away.
+		//Maybe use OnTriggerEnter to trigger wandering AI?
 	}
 
 	}
+
+	void OnCollisionEnter (Collision other) {
+		//"Collider" is used with "Trigger". "Collision" is used with "Collision".
+
+		if (other.gameObject.name == "Player") {
+			transform.position = chickenPen.position;
+			transform.rotation = chickenPen.rotation;
+
+			//If changed to other.transform, Player would be transported to Chicken Pen.
+		}
+	}
+
 }
--- Chicken_Game/Assets/Scripts/Wolf_AI.cs
+++ Chicken_Game/Assets/Scripts/Wolf_AI.cs
@@ -2,14 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent (typeof(CharacterController))]
 public class Wolf_AI : MonoBehaviour {
 
 	public Rigidbody wolf;
 	public float moveSpeed;
 	public Transform target;
 
+//Variables for wandering_AI
 
-void Random () {}
+	// public float directionChangeInterval = 1;
+	// public float maxHeadingCharge = 30;
+
+	// CharacterController controller;
+	// float heading;
+	// Vector3 targetRotation;
 
 void OnTriggerStay(Collider other) {
 
@@ -17,9 +24,36 @@ if (other.gameObject.name == "Player") {
 	transform.LookAt (target);
 	transform.Translate (Vector3.forward * moveSpeed * Time.deltaTime);
 }
-
 }
 
+// void Awake () {
+// 	controller = GetComponent<CharacterController>();
+
+// 	heading = Random.Range (0,360);
+// 	transform.eulerAngles = new Vector3 (0,heading,0);
+
+// 	StartCoroutine(NewHeading());
+// }
+
+// void Update () {
+// 	transform.eulerAngles = Vector3.Slerp (transform.eulerAngles , targetRotation , Time.deltaTime * directionChangeInterval);
+// 	var forward = transform.TransformDirection (Vector3.forward);
+// 	controller.SimpleMove (forward * moveSpeed);
+// }
+
+// IEnumerator NewHeading () {
+// 	while (true) {
+// 		NewHeadingRoutine ();
+// 		yield return new WaitForSeconds (directionChangeInterval);
+// 	}
+// }
+	
+// void NewHeadingRoutine () {
+// 	var floor = transform.eulerAngles.y - maxHeadingCharge;
+// 	var ceiling = transform.eulerAngles.y + maxHeadingCharge;
+
+// 	heading = Random.Range (floor , ceiling);
+// 	targetRotation = new Vector3 (0 , heading , 0);
+// }
+
 }
-
-
