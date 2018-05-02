565e4cabd3c8f42d60687cd2077184e40f5d18bc
Calysto Chastain <31229462+cchastainuvu@users.noreply.github.com>
2018-05-02 17:55:38 -0600

    unnecessary commented scripts removed

--- Chicken_Game/Assets/Scripts/Foxes/Fox_AI.cs
+++ Chicken_Game/Assets/Scripts/Foxes/Fox_AI.cs
@@ -6,18 +6,11 @@ public class Fox_AI : MonoBehaviour {
 
 public Transform foxSpawn;
 public Transform target;
-// public Transform target2;
-// public Transform target3;
-// public Transform target4;
-// public Transform target5;
 public float moveSpeed;
-// public int points = 5;
 public AudioSource foxCry;
 
 void Start () {
 	target = GameObject.FindWithTag("Chicken").transform;
-	// target2= GameObject.FindWithTag("Chicken").transform;
-	// target3= GameObject.FindWithTag("Chicken").transform;
 
 }
 
@@ -28,8 +21,6 @@ void OnTriggerStay (Collider other) {
 		transform.LookAt (target);
 		transform.Translate (Vector3.forward * moveSpeed * Time.deltaTime);
 		wanderScript.enabled = true;
-
-		// Score_Manager.TakePoints(points);
 	}
 
 }
@@ -41,16 +32,7 @@ public void OnCollisionEnter (Collision other) {
 		transform.position = foxSpawn.position;
 		transform.rotation = foxSpawn.rotation;
 	}
-	// if (other.gameObject.tag == "Chicken") {
-	// 	// var hit = other.gameObject;
-	// 	// var health = hit.GetComponent<Player_Health>();
-	// 	// print ("Wolf is attacking!");
-		
-	// 	// if (pcHealth != null) {
-	// 	// 	pcHealth.gameObject.GetComponent<Player_Health>().TakeDamage(damage);
-
-	// 	Destroy(other.gameObject);
-		// }
+	
 		if (other.gameObject.tag == "Checkpoint") {
 			Turn();
 	}
