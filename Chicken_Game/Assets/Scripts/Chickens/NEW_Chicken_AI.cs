e372b6afc74687eed6b8dd39345de84caf78183a
Calysto Chastain <31229462+cchastainuvu@users.noreply.github.com>
2018-05-02 17:49:57 -0600

    unneccessary commented scripts removed

--- Chicken_Game/Assets/Scripts/Chickens/NEW_Chicken_AI.cs
+++ Chicken_Game/Assets/Scripts/Chickens/NEW_Chicken_AI.cs
@@ -5,19 +5,11 @@ using UnityEngine.AI;
 
 public class NEW_Chicken_AI : MonoBehaviour {
 
-// public Rigidbody enemy;
-
-// Regular variables:
 public float moveSpeed;
 public Transform target;
 public Transform chickenPen;
-// public Transform chickenSpawn;
 public int points = 10;
 public int time = 4;
-
-// void Update () {
-// 	StartCoroutine(SpawnChicken());
-// }
 
 
 void OnTriggerStay (Collider other) {
@@ -50,9 +42,6 @@ void OnCollisionEnter (Collision other) {
 			Turn();
 	}
 	if (other.gameObject.tag == "Fox") {
-		// transform.position = chickenSpawn.position;
-		// transform.rotation = chickenSpawn.rotation;
-		// SpawnChicken();
 		Destroy(gameObject);
 	}
 
@@ -68,62 +57,5 @@ void Turn () {
 
 }
 
-// IEnumerator SpawnChicken () {
-// 	yield return new WaitForSeconds(time);
-// 	transform.position = chickenSpawn.position;
-// 	transform.rotation = chickenSpawn.rotation;
-// }
-
-
-
-
-
-
 }
-
-
-
-
-
-
-// void OnTriggerExit(Collider other) {
-// 	if (other.gameObject.tag == "Checkpoint") {
-// 		Turn();
-// 	}
-// 	// else if {
-// 	// if (other.gameObject.name )
-// 	// }
-// }
-// void Turn () {
-// 	int randomNum = Random.Range (0,360);
-// 	transform.Rotate (0,randomNum,0);
-// }
-
-// void MoveForward () {
-// 	transform.Translate (Vector3.forward * moveSpeed * Time.deltaTime);
-// }
-
-
-// void OnTriggerStay (Collider other) {
-
-// 	if (other.gameObject.name == "Player") {
-// 		Debug.Log ("Player has entered the chicken's trigger.");
-// 		// transform.LookAt (target);
-// 		transform.Translate (Vector3.forward * moveSpeed * Time.deltaTime);
-// 		transform.rotation = Quaternion.Inverse (target.rotation);
-// 	}
-
-// }
-
-// void OnCollisionEnter (Collision other) {
-
-// 		if (other.gameObject.name == "Player") {
-
-// 			Score_Manager.AddPoints(points);
-
-// 			transform.position = chickenPen.position;
-// 			transform.rotation = chickenPen.rotation;
-
-// 		}
-// }
 
