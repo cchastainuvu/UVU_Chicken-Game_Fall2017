41fa6c054f6fc4bd4aecd7dc654feb7399dfe126
Calysto Chastain <31229462+cchastainuvu@users.noreply.github.com>
2018-05-02 17:56:38 -0600

    unnecessary commented scripts removed

--- Chicken_Game/Assets/Scripts/Hat/Hat_Spawn.cs
+++ Chicken_Game/Assets/Scripts/Hat/Hat_Spawn.cs
@@ -7,19 +7,12 @@ public class Hat_Spawn : MonoBehaviour {
 public GameObject hat;
 public Transform [] hatSpawns;
 
-// public string what;
-
 void Start () {
 	Spawn();
 }
 
-// void FixedUpdate() {
-// 	transform.Rotate(0, 100f * Time.deltaTime, 0);
-// }
-
 void Spawn () {
         int spawnPointIndex = Random.Range (0,hatSpawns.Length);
-        // yield return new WaitForSeconds (spawnTime);
         Instantiate (hat, hatSpawns[spawnPointIndex].position, hatSpawns[spawnPointIndex].rotation);
     }
 }
