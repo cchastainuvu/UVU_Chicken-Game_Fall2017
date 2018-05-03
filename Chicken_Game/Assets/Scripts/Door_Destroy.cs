91c1230ba19815c7fe966020a63b55c1b98d8b45
Calysto Chastain <31229462+cchastainuvu@users.noreply.github.com>
2018-05-02 18:01:40 -0600

    script purpose added

--- Chicken_Game/Assets/Scripts/Door_Destroy.cs
+++ Chicken_Game/Assets/Scripts/Door_Destroy.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Door_Destroy : MonoBehaviour {
 
-
+//Barn door will appear to open in conjuction with other half of door.
 
 void OnTriggerEnter (Collider other) {
 	if (other.gameObject.name == "Player") {
