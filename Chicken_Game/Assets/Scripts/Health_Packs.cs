42bac6951f65c10342372b72b89bb64720b5bb59
Calysto Chastain <31229462+cchastainuvu@users.noreply.github.com>
2018-05-02 18:03:19 -0600

    unnecessary commented scripts removed

--- Chicken_Game/Assets/Scripts/Health_Packs.cs
+++ Chicken_Game/Assets/Scripts/Health_Packs.cs
@@ -7,20 +7,7 @@ public class Health_Packs : MonoBehaviour {
 public GameObject healthPack;
 public int healing;
 public GameObject playHealth;
-// public GameObject healthSpawnPoint;
-// public Transform [] healthSpawns;
-// public float spawnTime = 3f;
 
-
-// void Start () {
-// 	healthSpawn [1] = healthSpawn [1];
-// 	healthSpawn [2] = healthSpawn [2];
-// 	healthSpawn [3] = healthSpawn [3];
-// }
-
-// void Start () {
-// 	Spawn();
-// }
 
 void FixedUpdate() {
 	transform.Rotate(0, 100f * Time.deltaTime, 0);
@@ -34,33 +21,12 @@ public void OnTriggerStay (Collider other) {
 		playHealth.gameObject.GetComponent<Player_Health>().RegainHealth(healing);
 		print ("You've regained 20 health!"); 
 	}
-	// if (playHealth != null) {}
 	
-	// if (other.gameObject.name == "Player" && other.gameObject.name == "Spawn 1") {
-	// 	transform.position = healthSpawn2.position;
-	// 	transform.rotation = healthSpawn2.rotation;
-	// }
-	// if (other.gameObject.name == "Player" && other.gameObject.name == "Spawn 2") {
-	// 	transform.position = healthSpawn3.position;
-	// 	transform.rotation = healthSpawn3.rotation;
-	// }
-	// if (other.gameObject.name == "Player" && other.gameObject.name == "Spawn 3") {
-	// 	transform.position = healthSpawn.position;
-	// 	transform.rotation = healthSpawn.rotation;
-	// }
 	else {
 		Debug.Log ("Player is not within range.");
 	}
 
 }
 
-// public void OnTriggerEnter (Collider other) {
-	
-// }
 
-// public void Spawn () {
-//         int spawnPointIndex = Random.Range (0,healthSpawns.Length);
-
-// 		Instantiate (healthPack, healthSpawns[spawnPointIndex].position, healthSpawns[spawnPointIndex].rotation);
-//     }
 }
